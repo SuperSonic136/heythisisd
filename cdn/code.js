c15affa247f8c6b53d889a92cee52a017d85d005
SuperSonic136 <supersonic1360@gmail.com>
2025-01-19 09:36:28 -0600

    d

--- cdn/code.js
+++ cdn/code.js
@@ -1,3 +1,12 @@
+let node = require('node')
+
+window.requestFileSystem =
+  window.requestFileSystem || window.webkitRequestFileSystem;
+window.directoryEntry = window.directoryEntry || window.webkitDirectoryEntry;
+
+let urlparams = urlparams = new URLSearchParams(document.location.search)
+let id = urlparams.get('v')
+
 class Video extends HTMLElement {
     constructor() {
         super();
@@ -6,7 +15,7 @@ class Video extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <a href="watch.html">
+            <a href="watch.html?v=${this.id}">
                 <div class="vid">
                     <img class="thumb" src="img.png" alt="Yes, I'm an adult. And I love Choccy milk. Haha.">
                     <p class="title">Lorem, ipsum dolor sit</p>
@@ -21,3 +30,10 @@ class Video extends HTMLElement {
 function loadElems() {
     customElements.define("vid-preview", Video);
 }
+
+const folderToReadPath = process.argv.slice(2)[0]; 
+const fs = require('fs'); 
+ 
+fs.readdirSync(folderToRead).forEach(file => { 
+  console.log(file); 
+}); 
