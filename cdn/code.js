bb31150d0d2f59e303bebb6129a995d4ab5f0b51
SuperSonic136 <supersonic1360@gmail.com>
2025-01-20 19:25:21 -0600

    killing myuself

--- cdn/code.js
+++ cdn/code.js
@@ -1,33 +1,31 @@
-const fs = require('node:fs')
-
-window.requestFileSystem =
-  window.requestFileSystem || window.webkitRequestFileSystem;
-window.directoryEntry = window.directoryEntry || window.webkitDirectoryEntry;
-
-var urlparams = urlparams = new URLSearchParams(document.location.search)
-let id = urlparams.get('v')
-
 class Video extends HTMLElement {
     constructor() {
         super();
         this.id = this.getAttribute("vidid");
+        this.fetchinfo = fetch("cdn/videos/"+this.id+"/info.json")
+        this.info = JSON.parse(this.fetchinfo)
     }
 
     connectedCallback() {
+        
         this.innerHTML = `
             <a href="watch.html?v=${this.id}">
                 <div class="vid">
-                    <img class="thumb" src="img.png" alt="Yes, I'm an adult. And I love Choccy milk. Haha.">
-                    <p class="title">Lorem, ipsum dolor sit</p>
+                    <img class="thumb" src="cdn/videos/${this.id}/thumb.webp" alt="Yes, I'm an adult. And I love Choccy milk. Haha.">
+                    <p class="title">${this.info[title]}</p>
                     <p class="details">3 views</p>
                 </div>
             </a>
         `;
+        
     }
 }
 
+var urlparams = urlparams = new URLSearchParams(document.location.search)
+let id = urlparams.get('v')
+console.log(JSON.parse(fetch("cdn/videos/AuY0f0sdri8/info.json")))
 
 function loadElems() {
     customElements.define("vid-preview", Video);
 }
-console.log(window)
+
